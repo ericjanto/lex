05c29f26a8e1e5e7dfe72e7199b7796e0712b98e
Eric Janto <e.janto@gmx.de>
2023-02-18 22:32:49 +0000

    Clean up code

--- fileparser.py
+++ fileparser.py
@@ -5,6 +5,7 @@ File Parser
 Parses file contents into .txt-format.
 Supports epub only for now.
 """
+
 import ebooklib
 import json
 import os
@@ -37,39 +38,23 @@ class FileParser:
             case _:
                 raise NotImplementedError(
                     f"""
-                                        Parsing for files of type {self.file_extension}
-                                        is not implemented yet. If this is not
-                                        the expected file type, make sure the 
-                                        file name is correct.
-                                        """
+                    Parsing for files of type {self.file_extension}
+                    is not implemented yet. If this is not
+                    the expected file type, make sure the 
+                    file name is correct.
+                    """
                 )
 
     def parse_epub(self) -> None:
         """
-        TODO
+        Parses an epub file into .txt format, and
+        saves the corresponding metadata in a JSON file.
         """
-        self.book = epub.read_epub(self.path, {"ignore_ncx": True})
+        book = epub.read_epub(self.path, {"ignore_ncx": True})
 
-        with open(self.metapath, "w") as f:
-            m = {}
-            author_meta = self.book.get_metadata("DC", "creator")
-            if author_meta:
-                m["author"] = author_meta[0][1]["{http://www.idpf.org/2007/opf}file-as"]
-            else:
-                m["author"] = input("Please provide author manually: ")
-            m["title"] = self.book.get_metadata("DC", "title")[0][0]
-            m["language"] = self.book.get_metadata("DC", "language")[0][0]
-            while not re.match(r"\w{2}", m["language"]):
-                m["language"] = input(
-                    """
-                    Language specifier does not conform to
-                    shorthand ISO 639 (e.g. en). Please enter
-                    manually: 
-                    """
-                )
-            json.dump(m, f)
+        self.save_metadata_epub(book)
 
-        items = self.book.get_items_of_type(ebooklib.ITEM_DOCUMENT)
+        items = book.get_items_of_type(ebooklib.ITEM_DOCUMENT)
         items = filter(lambda i: i.get_name().find(".html") >= 0, items)
 
         with open(self.contentpath, "w") as f:
@@ -77,6 +62,29 @@ class FileParser:
                 soup = BeautifulSoup(item.get_body_content(), "html.parser")
                 for p in soup.find_all("p"):
                     f.write(p.get_text())
+
+    def save_metadata_epub(self, book):
+        """
+        Reads and saves metadata from an epub book.
+        """
+        with open(self.metapath, "w") as f:
+            m = {}
+            author_meta = book.get_metadata("DC", "creator")
+            if author_meta:
+                m["author"] = author_meta[0][1]["{http://www.idpf.org/2007/opf}file-as"]
+            else:
+                m["author"] = input("Please provide author manually: ")
+            m["title"] = book.get_metadata("DC", "title")[0][0]
+            m["language"] = book.get_metadata("DC", "language")[0][0]
+            while not re.match(r"\w{2}", m["language"]):
+                m["language"] = input(
+                    """
+                    Language specifier does not conform to
+                    ISO 639-1 (e.g. en). Please enter
+                    manually: 
+                    """
+                )
+            json.dump(m, f)
 
     def clean(self) -> None:
         """

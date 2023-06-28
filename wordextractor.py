436de4198428800f7e93d5cca58a5d9a419ebc0e
Eric Janto <e.janto@gmx.de>
2023-06-28 18:22:55 +0100

    Simplify tagging and lemmatisation

--- wordextractor.py
+++ wordextractor.py
@@ -11,12 +11,10 @@ Extracts vocabulary from a text by performing these steps:
 6) Return list of (lemma,PoS-tag,token)
 """
 
-from typing import List, NamedTuple, Tuple
+from typing import List, NamedTuple
 from nltk.stem import WordNetLemmatizer
-from nltk.tag import pos_tag_sents, pos_tag, map_tag
+from nltk.tag import pos_tag, map_tag
 from nltk.tokenize import word_tokenize
-
-from utils import iso_639_1_to_full, iso_639_1_to_639_2
 
 class TokenisedSentence(NamedTuple):
     original_file_line: int
@@ -26,23 +24,36 @@ class TaggedToken(NamedTuple):
     token: str
     tag: str
 
-class TaggedSentence(NamedTuple):
-    original_file_line: int
-    tokens: List[TaggedToken]
+class TaggedLemma(NamedTuple):
+    lemma: str
+    tag: str
 
-class WordExtractor:
+class LemmatisedSentence(NamedTuple):
+    original_file_line: int
+    lemmata: List[TaggedLemma]
+
+class LemmaExtractor:
     """
-    Vocabulary extractor
+    Lemma extractor
+
+    TODO: testing
+    TODO: what's the output you *need*? Are original line numbers still necessary?
+    TODO: currently uses PerceptronTagger, explained here: https://explosion.ai/blog/part-of-speech-pos-tagger-in-python
+            - Indicates that as a feature, it looks at i-2 and i+2 tags. This means that rather than
+              tagging each sentence individually, it'll be beneficial to parse sentences in context to
+              each other to increase tagging performance near sentence boundaries.
+            - Hence, use pos_tag_sents
+    TODO: automated formatting with black
+    TODO: documentation
+    TODO: text passed into lemmata
 
     Args:
         path_to_txt: path to text to extract words from
         language: language of the text, specified in ISO 639-1
     """
 
-    def __init__(self, path_to_txt: str, path_to_stopwords: str, language: str) -> None:
-        self.language_full = iso_639_1_to_full(language)
-        self.language_639_1 = language
-        self.language_639_2 = iso_639_1_to_639_2(language)
+    def __init__(self, path_to_txt: str, path_to_stopwords: str) -> None:
+        self.lemmatise = WordNetLemmatizer().lemmatize
 
         with open(path_to_txt, "r") as f:
             self.text = f.readlines()
@@ -50,24 +61,16 @@ class WordExtractor:
         with open(path_to_stopwords, "r") as f:
             self.stopwords = f.readlines()
 
-    def extract_vocabulary(self) -> List[Tuple[str, str]]:
-        """
-        TODO: docs
-        returns [(lemma,tag,original)] (use NamedTuple for this TODO)
-        """
-        tokenised_sentences = self._tokenise()
-        tagged_tokens = self._tag(tokenised_sentences)
-        pass
-
-    def _tokenise(self) -> List[TaggedSentence]:
+    def extract_lemmata(self) -> List[LemmatisedSentence]:
         """
         Tokenises text whilst preserving sentence context.
         Also removes stopwords.
+        TODO: docs
 
         Returns:
             list of TokenisedSentences
         """
-        tagged_sentences = []
+        lemmatised_sentences = []
         for line_num,sentence in enumerate(self.text):
             # Tokenisation
             if tokens := word_tokenize(sentence, language=self.language_full):
@@ -91,33 +94,22 @@ class WordExtractor:
                 )
                 relevant_tokens = list(filter(lambda x: x.tag in relevant_universal, tagged_tokens))
 
-                # Lemmatisation (TODO)
+                # Lemmatisation
+                tagmap_universal_lemmatiser = {
+                    "VERB": "v",
+                    "NOUN": "n",
+                    "ADJ": "a",
+                    "ADV": "r",
+                }
 
-                tagged_sentences.append(TaggedSentence(line_num, relevant_tokens))
+                tagged_lemmata = []
+                pos_default = 'n'
+                for rt in relevant_tokens:
+                    lemmatag = None
+                    if rt.tag in tagmap_universal_lemmatiser:
+                        lemmatag = tagmap_universal_lemmatiser[rt.tag]
+                    lemma = self.lemmatise(rt.token, pos=lemmatag or pos_default)
+                    tagged_lemmata.add(TaggedLemma(lemma, lemmatag or pos_default))
 
-        return tagged_sentences
-
-    def _tag(self, tokenised_sentences: List[TokenisedSentence]) -> List[TaggedSentence]:
-        """
-        Tags the given tokenised sentences using a language-dependent
-        tagger and tagset.
-
-        Args:
-            tokenised_sentences: sentences containing the tokens to be tagged
-        
-        Returns:
-            list of TaggedSentences
-        """
-        lines = [t.original_file_line for t in tokenised_sentences]
-        sent_tokens = [t.tokens for t in tokenised_sentences]
-        tagged_sentences = [[TaggedToken(token,tag) for (token,tag) in tagged_sent] for tagged_sent in pos_tag_sents(sent_tokens)]
-        return [TaggedSentence(l,ts) for l,ts in zip(lines, tagged_sentences)]
-
-    def _lemmatise(self):
-        # Is this also language dependent?
-        pass
-
-
-    def _map_tags(self):
-        # Use nltk's map_tag()
-        pass
+                lemmatised_sentences.append(LemmatisedSentence(line_num, tagged_lemmata))
+        return lemmatised_sentences

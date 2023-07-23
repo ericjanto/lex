a54cd60fcd33f7ba8fbbfaaeab2c6f0d3a0d70fa
Eric Janto <e.janto@gmx.de>
2023-07-23 18:42:38 +0100

    build: try import of enclosing folder for deploy build

--- frontend/api/index.py
+++ frontend/api/index.py
@@ -1,3 +1,4 @@
+from backend.utils import relativy_path
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,8 @@ app = FastAPI()
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+
+@app.get("/api/import")
+def import_test():
+    return {"message": relativy_path("he!")}

72ce6a2de5fd177a74c108286ca34eb9f7d77dd2
Eric Janto <e.janto@gmx.de>
2023-07-24 19:46:30 +0100

    fix: check we're deplopying the original folder

--- backend/app/page.js
+++ backend/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
+          <code className={styles.code}>api/index.py</code>
         </p>
         <div>
           <a

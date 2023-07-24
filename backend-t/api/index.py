b47a0db24a970a1b03a3e0e48dcfbe477f1ace76
Eric Janto <e.janto@gmx.de>
2023-07-24 12:43:54 +0100

    build: try old path :')

--- backend-t/api/index.py
+++ backend-t/api/index.py
@@ -5,4 +5,9 @@ app = FastAPI()
 
 @app.get("/api/python")
 async def hello_world():
-    return {"messsage": "hello world"}
+    return {"messsage": "hello python"}
+
+
+@app.get("/api/haskell")
+async def hello_haskell():
+    return {"messsage": "hello haskell"}

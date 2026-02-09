73c338dcd9adf308426d2e7a6b1da310eac3f4b4
ericjanto <jantoeric@gmail.com>
2026-02-09 13:09:56 +0100

    fix: use async server component

--- frontend/src/app/context/[contextId]/page.tsx
+++ frontend/src/app/context/[contextId]/page.tsx
@@ -1,7 +1,8 @@
-export default function Page({ params }: { params: { contextId: string } }) {
+export default async function Page({ params }: { params: Promise<{ contextId: string }> }) {
+  const { contextId } = await params;
   return (
     <>
-      <h1>Context {params.contextId}</h1>
+      <h1>Context {contextId}</h1>
       <div>Dummy page, to be implemented.</div>
     </>
   );
